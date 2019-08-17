f8039b01bae51b338f3998f90023e3eded00a5fd
1127883584@qq.com <1127883584@qq.com>
2019-08-17 15:25:21 +0800

    should_return_player2_win_when_player1_is_3H_3C_3D_6H_6C_and_player2_is_5C_5S_5D_4S_4D

--- src/test/java/pokers/PokerTest.java
+++ src/test/java/pokers/PokerTest.java
@@ -217,4 +217,18 @@ public class PokerTest {
         //Then
         Assert.assertEquals(player1, winner);
     }
+
+    @Test
+    public void should_return_player2_win_when_player1_is_3H_3C_3D_6H_6C_and_player2_is_5C_5S_5D_4S_4D(){
+        //Given
+        List player1 = Arrays.asList(new Poker("3H"), new Poker("3C"), new Poker("3D"), new Poker("6H"), new Poker("6C"));
+        List player2 = Arrays.asList(new Poker("5C"), new Poker("5S"), new Poker("5D"), new Poker("4S"), new Poker("4D"));
+
+        //When
+        PokerHandler pokerHandler = new PokerHandler();
+        List winner = pokerHandler.handle(player1, player2);
+
+        //Then
+        Assert.assertEquals(player2, winner);
+    }
 }
--- src/test/java/pokers/tasking.md
+++ src/test/java/pokers/tasking.md
@@ -109,3 +109,10 @@
 （3H_3C_3D_6H_6C） ，（3S_5S_6S_7S_8S）
 （1 player win）
 `
+
+### 17.player1 有俘虏，player2有俘虏
+##### should_return_player2_win_when_player1_is_3H_3C_3D_6H_6C_and_player2_is_5C_5S_5D_4S_4D
+`
+（3H_3C_3D_6H_6C） ，（5C_5S_5D_4S_4D）
+（2 player win）
+`
